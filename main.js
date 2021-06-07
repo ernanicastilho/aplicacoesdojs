d971ccf9ed1a3b628f40c6d4f12d7c7290744fcb
unknown <ernanicastilhom@gmail.com>
2021-06-07 03:49:20 -0300

    java

---
+++ main.js
@@ -0,0 +1,37 @@
+
+   // function redirecionar(){
+//window.open("https://ernanicastilho.github.io/instagram/");
+
+function clicou() {
+    alert("Obrigado por clicar");
+}
+var frase ="Jesus e o melhor professor";
+alert(frase.toUpperCase());
+
+var lista = ["maça", "pera","laranja"];
+lista.push("pera");
+alert(lista[1]);
+
+var fruta = {nome:"maça", cor:"vermelha"}
+alert(fruta.cor);
+
+var idade = prompt("qual sua idade");
+if (idade >= 18){
+    alert("maior de idade");
+} else { alert("menor de idade"); };
+
+var d = new Date();
+alert(d);
+
+var d = new Date();
+alert(d.getDay());
+
+
+
+var count = 0;
+while (count <= 2){
+    console.log(count);
+    alert(count);
+    count++;
+};
+/**/
